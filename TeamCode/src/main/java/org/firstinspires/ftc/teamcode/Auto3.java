2c75fe90e3f6984f5a00bab4848a5f93666b469a
gronkybong <48972370+gronkybong@users.noreply.github.com>
2022-04-02 11:30:02 -0700

    finite state machine branch kinda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto3.java
@@ -20,11 +20,15 @@ public class Auto3 extends LinearOpMode {
 //        int[] pos;
 //        pos = robot.detect();
         if (true) {
+            // STATUS_EXTENDING
             robot.goToIntakePosition(3);
+            // ?? Rotating turrent ?
             robot.setExtension(robot.maxExtension);
             robot.waitOnExtension(robot.maxExtension);
             robot.sleep(100);
+            // CONDITION : wait for extension + 100ms
 
+            // STATUS_RELEASING_RETRACTING_DRIVING
             robot.goToFlipperPosition(0);
             robot.extensionCheckpoints[3] = true;
             robot.setExtension(robot.minExtension);
@@ -35,12 +39,22 @@ public class Auto3 extends LinearOpMode {
             robot.driveByGyroWithEncodersVertical(robot.DIRECTION_FORWARD, 35000, false, 500, 0);
             robot.goToAngle(0, 0.16);
             robot.sleep(300);
-            
+            // CONDITION : wait for driving + 300ms
+
+            // STATUS_INTAKING
             robot.autoGrabFreight(0.2);
+            // CONDITION : distance sensor detected freight
+            // STATUS_DRIVING_OUT_WAREHOUSE
 
             robot.goToAngle(0, 0.16);
+            // CONDITION : wait for driving
+            // STATUS_EXTENDING
             robot.waitOnExtension(robot.maxExtension);
             robot.sleep(1000);
+            // CONDITION : wait for extension + 1000ms
+
+
+            // STATUS_RELEASING_RETRACTING_DRIVING
             robot.goToFlipperPosition(0);
             robot.extensionCheckpoints[3] = true;
             robot.setExtension(robot.minExtension);
@@ -48,7 +62,7 @@ public class Auto3 extends LinearOpMode {
 
             robot.driveByGyroWithEncodersVertical(robot.DIRECTION_FORWARD, 45000, false, 500, 0);
             robot.goToAngle(0, 0.16);
-
+            // CONDITION : wait for driving
 
             robot.sleep(10000);
             //robot.driveByGyroWithEncodersVertical(robot.DIRECTION_BACKWARD, 10000, 0.6, false, false, 200);
